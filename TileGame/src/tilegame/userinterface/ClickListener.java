9164aefbc7f72169f59ed8e4a25025b20681c682
Acer-MARIAN <Acer-MARIAN@Acer-MARIAN-PC>
2018-12-21 14:29:04 +0100

    schtragglnachtrag

--- TileGame/src/tilegame/userinterface/ClickListener.java
+++ TileGame/src/tilegame/userinterface/ClickListener.java
@@ -1,7 +1,7 @@
 package tilegame.userinterface;
 
 //Interface that together with the mouseInputManager enables mouse input
-// and will pass on its method to subclasses
+// and will pass on its method to subclasses, is implemented in the buttonclasses wher the onclick method is overwritten
 public interface ClickListener {
 
 	public void onClick(); 
